52fa69111f926c2db7950fecd84a1625916a6df6
MounikaBompelly <141317409+MounikaBompelly@users.noreply.github.com>
2023-08-17 15:58:18 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+const Footer = ()  => {
+    return (
+        <>
+       <img src={logo} alt="my logo" height={70}/>
+        </>
+    )
+        
+}
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+const Header = ()  => {
+    return (
+        <>
+        <header className="App-header">
+        <h1>Welcome to Global Logic Mounika!!</h1>
+        
+        <a
+          className="App-link"
+          href="https://www.globallogic.com/in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn more about GL
+        </a>
+      </header>
+        </>
+    )
+        
+}
+export default Header;
